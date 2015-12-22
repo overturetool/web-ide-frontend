fd496a56d85a42b0b27c144680fb65c4b55f98ae
Rasmus Reimer <rasmus.reimer@gmail.com>
2015-12-22 01:22:25 +0100

    stuff

--- app/files/files.component.ts
+++ app/files/files.component.ts
@@ -14,14 +14,18 @@ export class FilesComponent {
     constructor(el:ElementRef, files:FilesService) {
         this.$container = $(el.nativeElement).find(".container").first();
 
-        this.$container.jstree({
-            "state": {"key": "files"},
-            "plugins": ["contextmenu", "dnd", "search", "sort", "state", "wholerow"],
-            "core": {
-                "animation": false,
-                "check_callback": true
-            }
-        });
+        this.$container
+            .on('activate_node.jstree', function (e, data) {
+                //editor.open(data.node.original.path);
+            })
+            .jstree({
+                "state": {"key": "files"},
+                "plugins": ["contextmenu", "dnd", "search", "sort", "state", "wholerow"],
+                "core": {
+                    "animation": false,
+                    "check_callback": true
+                }
+            });
 
         files.readDir("", 10)
             .forEach(response => {
@@ -32,8 +36,9 @@ export class FilesComponent {
 
     dirToJsTree(dir) {
         return dir.map(file => {
-            var node = {
+            var node:any = {
                 text: file.name,
+                path: file.url,
                 icon: file.type === "directory" ? "jstree-folder" : "jstree-file"
             };
 
