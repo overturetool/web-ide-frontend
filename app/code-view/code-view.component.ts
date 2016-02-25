8a621d5323350309ce04d5ab02f46abd8e0ae5b4
Rasmus Reimer <rasmus.reimer@gmail.com>
2016-02-25 10:26:30 +0100

    Fix multilime code view.

--- app/code-view/code-view.component.ts
+++ app/code-view/code-view.component.ts
@@ -1,21 +1,34 @@
 import {Component, Input, ElementRef} from "angular2/core";
+import callSignatureElement = ts.ScriptElementKind.callSignatureElement;
 
 @Component({
     selector: "code-view",
     template: ""
 })
 export class CodeViewComponent {
-    @Input() set code(code:string) {
-        if (code === undefined) return;
+    @Input()
+    set code(code:string) {
+        code = typeof code === "string" ? code : code.toString();
 
-        CodeMirror.runMode(
-            typeof code === "string" ? code : code.toString(),
-            "vdm",
-            this.el.nativeElement
-        );
+        if (code.indexOf("\n") !== -1) {
+            var pre = document.createElement('pre');
+
+            CodeMirror.runMode(code, "vdm", (text, type) => {
+                var span = document.createElement('span');
+
+                span.innerHTML = text;
+                if (type) span.className = `cm-${type}`;
+
+                pre.appendChild(span);
+            });
+
+            this.el.nativeElement.appendChild(pre);
+        } else {
+            CodeMirror.runMode(code, "vdm", this.el.nativeElement);
+        }
     }
 
     constructor(private el:ElementRef) {
 
-    }
+}
 }
