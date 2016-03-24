b7e6a46202779aae5c69179e4c475607c597dca0
Rasmus Reimer <rasmus.reimer@gmail.com>
2016-03-24 11:00:31 +0100

    Fix bug.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,18 @@
 import {Component} from "angular2/core";
-import {AuthService} from "./auth/AuthenticationService";
 import {HTTP_PROVIDERS} from "angular2/http";
 import {ServerService} from "./server/ServerService";
 import {RouteConfig} from "angular2/router";
 import {ROUTER_PROVIDERS} from "angular2/router";
 import {ROUTER_DIRECTIVES} from "angular2/router";
 import {IdeComponent} from "./ide/ide.component";
-import {LoginComponent} from "./auth/login.component";
+import {AuthenticationService} from "./authentication/AuthService";
+import {LoginComponent} from "./authentication/login.component";
 
 @Component({
     selector: "app",
     template: "<router-outlet></router-outlet>",
     directives: [ROUTER_DIRECTIVES],
-    providers: [ROUTER_PROVIDERS, HTTP_PROVIDERS, ServerService, AuthService]
+    providers: [ROUTER_PROVIDERS, HTTP_PROVIDERS, ServerService, AuthenticationService]
 })
 @RouteConfig([
     {path: '/ide', name: 'Ide', component: IdeComponent},
