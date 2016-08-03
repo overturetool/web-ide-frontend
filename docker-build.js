01fac62c1636349481f237415f63c1d644e7efa0
Kasper Saaby <kdsaaby@gmail.com>
2016-08-03 10:35:47 +0200

    Changed production ip

--- docker-build.js
+++ docker-build.js
@@ -1,4 +1,4 @@
 var fs = require('fs');
 var file = './app/server/ServerService.ts';
 
-fs.writeFileSync(file, fs.readFileSync(file, "utf8").replace('"localhost:9000"', '"178.62.16.226:9000"'));
+fs.writeFileSync(file, fs.readFileSync(file, "utf8").replace('"localhost:9000"', '"130.225.20.6:9000"'));
